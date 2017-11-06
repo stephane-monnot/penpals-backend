062cb2aac4b9e21560ea9a743e6673a8cff5eb16
Monnot Stéphane <monnot.stephane@gmail.com>
2017-11-06 19:18:07 +0100

    Add spec for message model

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Validation tests
+  # ensure columns title and created_by are present before saving
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:content) }
+  it { should validate_presence_of(:created_by) }
 end
