7cc19981302ee9b5838b71b3967724ec96dab63b
Monnot Stéphane <monnot.stephane@gmail.com>
2017-11-09 19:08:57 +0100

    Add docker files and change database to postgres

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ end
 gem 'rails', '~> 5.1.4'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
+gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171107101449) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.string "title"
     t.text "content"
