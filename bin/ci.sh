b17b077e8a13fef17a535375d5f552ae554e6bf2
Monnot Stéphane <monnot.stephane@gmail.com>
2017-11-10 00:17:20 +0100

    Fix qa

---
+++ bin/ci.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# --build: Build images before starting containers.
+# --abort-on-container-exit: Stops all containers if any container is stopped
+docker-compose -f "docker-compose.yml" -f "docker-compose.ci.yml" up --abort-on-container-exit
