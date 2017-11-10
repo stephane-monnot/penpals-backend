54adc0f9b1a7e0ccb847e2cd6fd1d79d1f9526ca
Monnot Stéphane <monnot.stephane@gmail.com>
2017-11-10 17:41:13 +0100

    Try to fix build

--- bin/ci.sh
+++ bin/ci.sh
@@ -2,4 +2,4 @@
 
 # --build: Build images before starting containers.
 # --abort-on-container-exit: Stops all containers if any container is stopped
-docker-compose -f "docker-compose.yml" -f "docker-compose.ci.yml" up --build --abort-on-container-exit
+docker-compose -f "docker-compose.yml" -f "docker-compose.ci.yml" up --abort-on-container-exit
