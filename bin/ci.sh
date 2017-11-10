52bb677e7f7c01acfbe9e81e031c0ac5fb480535
Monnot Stéphane <monnot.stephane@gmail.com>
2017-11-11 00:36:34 +0100

    Try to fix build

--- bin/ci.sh
+++ bin/ci.sh
@@ -3,3 +3,6 @@
 # --build: Build images before starting containers.
 # --abort-on-container-exit: Stops all containers if any container is stopped
 docker-compose -f "docker-compose.yml" -f "docker-compose.ci.yml" up --abort-on-container-exit
+docker-compose run --rm web rails db:create
+docker-compose run --rm web rails db:migrate
+docker-compose run --rm web rspec
