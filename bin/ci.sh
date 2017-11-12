5d7a0ad39be5531aa7cc1c2facab0e70873f96f9
Monnot Stéphane <monnot.stephane@gmail.com>
2017-11-12 18:28:16 +0100

    Run bundle before run tests

--- bin/ci.sh
+++ bin/ci.sh
@@ -3,6 +3,7 @@
 # --build: Build images before starting containers.
 # --abort-on-container-exit: Stops all containers if any container is stopped
 docker-compose -f "docker-compose.yml" -f "docker-compose.ci.yml" up --abort-on-container-exit
+docker-compose run --rm web bundle
 docker-compose run --rm web rails db:create
 docker-compose run --rm web rails db:migrate
 docker-compose run --rm web rspec
--- bin/deploy_staging.sh
+++ bin/deploy_staging.sh
@@ -3,5 +3,6 @@
 # --build: Build images before starting containers.
 # --abort-on-container-exit: Stops all containers if any container is stopped
 docker-compose -f "docker-compose.yml" -f "docker-compose.staging.yml" up -d
+docker-compose -f "docker-compose.yml" -f "docker-compose.staging.yml" run --rm web bundle
 docker-compose -f "docker-compose.yml" -f "docker-compose.staging.yml" run --rm web rails db:create
 docker-compose -f "docker-compose.yml" -f "docker-compose.staging.yml" run --rm web rails db:migrate
