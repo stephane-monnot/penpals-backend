2fbca8420bb256cfb110cb794495ef42b6f4ce84
Monnot Stéphane <monnot.stephane@gmail.com>
2017-11-12 18:38:21 +0100

    Rebuild image before run tests

--- bin/ci.sh
+++ bin/ci.sh
@@ -2,7 +2,7 @@
 
 # --build: Build images before starting containers.
 # --abort-on-container-exit: Stops all containers if any container is stopped
-docker-compose -f "docker-compose.yml" -f "docker-compose.ci.yml" up --abort-on-container-exit
+docker-compose -f "docker-compose.yml" -f "docker-compose.ci.yml" up --build --abort-on-container-exit
 docker-compose run --rm web bundle
 docker-compose run --rm web rails db:create
 docker-compose run --rm web rails db:migrate
